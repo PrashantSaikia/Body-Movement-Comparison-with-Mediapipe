4dbecdd24f4db23f94fc38760b2fa8a1b6d14488
Prashant Saikia <39755678+PrashantSaikia@users.noreply.github.com>
2023-11-13 12:26:50 +0000

    Update pose_module.py

--- pose_module.py
+++ pose_module.py
@@ -1,4 +1,4 @@
-import cv2
+import cv2, time, math
 import mediapipe as mp
 import time
 import math
@@ -6,7 +6,7 @@ import math
 class poseDetector():
  
     def __init__(self, mode=False, upBody=False, smooth=True,
-                 detectionCon=0.85, trackCon=0.85):
+                 detectionCon=0.7, trackCon=0.7):
  
         self.mode = mode
         self.upBody = upBody
@@ -16,16 +16,29 @@ class poseDetector():
  
         self.mpDraw = mp.solutions.drawing_utils
         self.mpPose = mp.solutions.pose
-        self.pose = self.mpPose.Pose(self.mode, min_detection_confidence=detectionCon, min_tracking_confidence=trackCon)
 
+        self.pose = self.mpPose.Pose(self.mode, min_detection_confidence=detectionCon, min_tracking_confidence=trackCon)
+ 
     def findPose(self, img, draw=True):
         imgRGB = cv2.cvtColor(img, cv2.COLOR_BGR2RGB)
         self.results = self.pose.process(imgRGB)
-        if self.results.pose_landmarks:
-            if draw:
-                self.mpDraw.draw_landmarks(img, self.results.pose_landmarks,
-                                           self.mpPose.POSE_CONNECTIONS)
+        if self.results.pose_landmarks and draw:
+            for id, lm in enumerate(self.results.pose_landmarks.landmark):
+                if id not in [1,2,3,4,5,6,7,8,9,10,17,18,19,20,]:  # Skip facial landmarks and fingers
+                    h, w, c = img.shape
+                    cx, cy = int(lm.x * w), int(lm.y * h)
+                    cv2.circle(img, (cx, cy), 5, (255, 0, 0), cv2.FILLED)
+
+            # Manually draw connections, avoiding facial landmarks
+            for conn in self.mpPose.POSE_CONNECTIONS:
+                if conn[0] > 10 and conn[1] > 10:  # Exclude connections involving facial landmarks
+                    pt1 = self.results.pose_landmarks.landmark[conn[0]]
+                    pt2 = self.results.pose_landmarks.landmark[conn[1]]
+                    x1, y1 = int(pt1.x * w), int(pt1.y * h)
+                    x2, y2 = int(pt2.x * w), int(pt2.y * h)
+                    cv2.line(img, (x1, y1), (x2, y2), (255, 255, 255), 3)
         return img
+
  
     def findPosition(self, img, draw=True):
         self.lmList = []
@@ -74,9 +87,11 @@ def main():
         success, img = cap.read()
         img = detector.findPose(img)
         lmList = detector.findPosition(img, draw=False)
+
+        lm_to_print = 20
         if len(lmList) != 0:
-            print(lmList[14])
-            cv2.circle(img, (lmList[14][1], lmList[14][2]), 15, (0, 0, 255), cv2.FILLED)
+            print(lmList[lm_to_print])
+            cv2.circle(img, (lmList[lm_to_print][1], lmList[lm_to_print][2]), 15, (0, 0, 255), cv2.FILLED)
  
         cTime = time.time()
         fps = 1 / (cTime - pTime)
